f0cff59e820a3a8405b7167d198216ae85e012da
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2022-02-12 09:39:32 +0900

    lib.rs error fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
+#[macro_use]
+extern crate fixed_hash;
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate serde;
+extern crate symbol_crypto_core as crypto;
+
+pub use self::clients::*;
+pub use self::helpers::*;
+pub use self::model::*;
+
+mod clients;
+mod core;
+mod helpers;
+mod model;
