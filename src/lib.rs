3d3122e78fec5f43e7f8d5cab6f199d174444d0f
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2022-02-12 11:17:07 +0900

    uint64

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+#[allow(unused_imports)]
 #[macro_use]
 extern crate fixed_hash;
 #[cfg(test)]
+#[allow(unused_imports)]
 #[macro_use]
 extern crate lazy_static;
+#[allow(unused_imports)]
 #[macro_use]
 extern crate serde;
 extern crate symbol_crypto_core as crypto;
---
+++ src/model/mod.rs
@@ -0,0 +1,4 @@
+pub use self::uint64::*;
+
+#[allow(dead_code)]
+mod uint64;
